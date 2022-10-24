267195e9cab311ab4a280ecccd3c9a00a248e87f
Sandra Molina Palencia <sandra.molina.palencia@and.digital>
2022-10-24 16:37:20 +0100

    adds list items to home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h2>HomePage</h2>
+      <ListGroup horizontal>
+        <ListGroup.Item>Existing Projects</ListGroup.Item>
+        <ListGroup.Item>Cretate New Project</ListGroup.Item>
+        <ListGroup.Item>Project Reviews</ListGroup.Item>
+        <ListGroup.Item>Archived Projects</ListGroup.Item>
+        <ListGroup.Item>User Search</ListGroup.Item>
+      </ListGroup>
+    </div>
+  );
 };
 
 export default HomePage;
