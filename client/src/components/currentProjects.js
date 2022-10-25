81aa371ba8f5fcfe33e4640b69f147aab43e442b
Sandra Molina Palencia <sandra.molina.palencia@and.digital>
2022-10-25 14:11:07 +0100

    adds styling to btns

--- client/src/components/currentProjects.js
+++ client/src/components/currentProjects.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 function CurrentProjects() {
   return (
     <section>
       <NavLink to="/">
-        <button>Close</button>
+        <Button variant="dark">Close</Button>
       </NavLink>
       <article>A live project will be displayed here</article>
       <article>A live project will be displayed here</article>
