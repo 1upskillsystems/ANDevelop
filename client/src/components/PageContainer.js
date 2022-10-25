b64268391364a5488362c2eddf34ef0339eab768
caitlinrwAND <caitlin.whiteley@and.digital>
2022-10-25 14:23:41 +0100

    create page container and add to pages

---
+++ client/src/components/PageContainer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { NavLink } from "react-router-dom";
+import "./css/pageContainer.css";
+
+const PageContainer = ({ pageTitle, buttonText, buttonTo, children }) => {
+  return (
+    <div id="page">
+      <header id="page-header">
+        <div id="and-develop">ANDevelop</div>
+        <h1 id="page-title">{pageTitle}</h1>
+      </header>
+      {buttonText && buttonTo && (
+        <NavLink to={buttonTo} id="button">
+          <Button variant="dark">{buttonText}</Button>
+        </NavLink>
+      )}
+      <main id="outer-container">
+        <div id="inner-container">{children}</div>
+      </main>
+    </div>
+  );
+};
+
+export default PageContainer;
