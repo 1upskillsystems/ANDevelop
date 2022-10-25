eb7ed69268ac88d57a8cc88cc756f629bf078053
Sandra Molina Palencia <sandra.molina.palencia@and.digital>
2022-10-25 14:09:37 +0100

    adds close btn

--- client/src/components/currentProjects.js
+++ client/src/components/currentProjects.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function CurrentProjects() {
   return (
     <section>
+      <NavLink to="/">
+        <button>Close</button>
+      </NavLink>
       <article>A live project will be displayed here</article>
       <article>A live project will be displayed here</article>
       <article>A live project will be displayed here</article>
