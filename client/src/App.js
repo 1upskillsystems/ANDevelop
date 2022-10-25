ba34cd2da903267efc7d011bde3d6f1f90284389
Caelyb <78689052+Caelyb@users.noreply.github.com>
2022-10-25 14:49:48 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { Form, Route, Routes } from "react-router-dom";
-import CurrentProjects from "./components/currentProjects";
-import CreateProjectButton from "./components/newProjectButton";
-import TemplateRoute from "./components/templateRoute";
-import Userlist from "./components/userSearch";
- 
-=======
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import TemplateRoute from "./components/templateRoute";
@@ -24,29 +16,12 @@ import Test from "./Test";
 import "./App.css";
 import CurrentProjects from "./components/currentProjects";
 
->>>>>>> main
 const App = () => {
   const loginContextValue = useUserContextValue();
   const projectContextValue = useProjectContextValue();
   const templateContextValue = useTemplateContextValue();
 
   return (
-<<<<<<< HEAD
-    <main>
-      <header>
-        <h1>ANDevelop</h1>
-        <CreateProjectButton />
-      </header>
-      <Routes>
-        <Route exact path="/" element={<CurrentProjects />} />
-        <Route path="/choose-template-route" element={<TemplateRoute />} />
-      </Routes>
-      <div>
-      <Userlist/>
-      </div>
-    </main>
-
-=======
     <UserContext.Provider value={loginContextValue}>
       <ProjectContext.Provider value={projectContextValue}>
         <TemplateContext.Provider value={templateContextValue}>
@@ -71,8 +46,7 @@ const App = () => {
         </TemplateContext.Provider>
       </ProjectContext.Provider>
     </UserContext.Provider>
->>>>>>> main
   );
 };
- 
-export default App;export default App;
+
+export default App;
