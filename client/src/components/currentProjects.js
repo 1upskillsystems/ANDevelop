ab6ee1f0c136e6274975a5a8169dd533ec1fcc0b
Cameron McDougall <cameron.mcdougall@and.digital>
2022-10-26 15:59:36 +0100

    fixed issues with pull db data

--- client/src/components/currentProjects.js
+++ client/src/components/currentProjects.js
@@ -1,6 +1,4 @@
-import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
-import Button from "react-bootstrap/Button";
+import React, { useContext, useEffect } from "react";
 import Accordion from 'react-bootstrap/Accordion';
 import PageContainer from "./PageContainer";
 import { ProjectContext } from "../contexts/projectContext";
@@ -14,17 +12,14 @@ function CurrentProjects() {
   // Used to search and return template obj through matching template ID
   const findTemplate = id => {
     let matchedTemp = null;
-    templates.templates.forEach(elem => (elem.id === id) ? matchedTemp = elem : null);
+    templates.templates.forEach(elem => elem.template_id === id ? matchedTemp = elem : null);
     return matchedTemp;
   }
 
   const calcTime = (started, deadline) => {
-    const startedUnix = Math.floor(new Date(started).getTime() / 1000);
-    const deadlineUnix = Math.floor(new Date(deadline).getTime() / 1000);
-    const currentUnix = Math.floor(Date.now() / 1000);
-
-    const total = +deadlineUnix - +startedUnix;
-    const elaps = currentUnix - startedUnix;
+    const current = Math.floor(Date.now() / 1000);
+    const total = +deadline - +started;
+    const elaps = current - started;
     
     return Math.round((elaps / total) * 100) + "%";
   }
@@ -38,14 +33,17 @@ function CurrentProjects() {
       <section>
         <Accordion>
           {projects.projects.map((proj, i) => {
+            
             if (proj.project_status === 1) {
               const currentTemplate = findTemplate(proj.template_id);
               const progress = calcTime(proj.time_start, proj.time_deadline);
+
               return (
                 <Accordion.Item eventKey="0" key={i}>
                     <Accordion.Header>
                       <div className="current-projects-header-info">
-                        <div>{currentTemplate.template_name}</div>
+                        <div>Project name: {proj.project_name} - (id: {proj.project_id})</div>
+                        <div>Template name: {currentTemplate.template_name}</div>
                         <div className="progress-wrapper">
                           <div className="progress-bar" style={{width: progress}}></div>
                         </div>
