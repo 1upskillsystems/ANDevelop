ac955d675b053817f760cc1d024b15aa7f4a9a49
Caelyb <78689052+Caelyb@users.noreply.github.com>
2022-10-26 11:54:53 +0100

    Back button
    back out of user search

--- client/src/components/userSearch.js
+++ client/src/components/userSearch.js
@@ -1,10 +1,15 @@
 import React from "react";
 import SearchBar from "./searchBar";
+import { NavLink } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 function UserSearch() {
   return (
     
      <div classname='App'>
+        <NavLink to="/">
+          <Button variant="dark">Close</Button>
+        </NavLink>
         <h1>User Search</h1>
        <SearchBar/>
      </div>
