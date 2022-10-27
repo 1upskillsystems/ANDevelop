557572c60b2bc0829034e16dfc2496b40bcd888a
Sandra Molina Palencia <sandra.molina.palencia@and.digital>
2022-10-27 11:04:44 +0100

    changes logic to consume service

--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -1,123 +1,122 @@
-import React, {useState} from 'react'
+import React, { useContext, useState } from "react";
 import { UserContext } from "../contexts/userContext";
 
+const SearchBar = ({ handleClick }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const [usersToDisplay, setUserToDisplay] = useState([]);
 
-const SearchBar = ({handleClick}) => {
-    const [searchInput, setSearchInput] = useState("");
-    const [usersToDisplay, setUserToDisplay] = useState([])
+  const users = useContext(UserContext);
 
-// const users = UserContext(UserContext);
+  // mock user data
+  // const users = [
+  //     {
+  //       id: 1,
+  //       first_name: "Caelyb",
+  //       last_name: "str",
+  //       clubs_house: "Somerville",
+  //       individual_role: "str",
+  //       projects: [
+  //         { project_id: "int", role: "int" },
+  //         { project_id: "int", role: "int" },
+  //       ],
+  //     },
+  //     {
+  //       id: 2,
+  //       first_name: "Andrew",
+  //       last_name: "str",
+  //       clubs_house: "Almeida",
+  //       individual_role: "str",
+  //       projects: [
+  //         { project_id: "int", role: "int" },
+  //         { project_id: "int", role: "int" },
+  //       ],
+  //     },
+  //     {
+  //         id: 2,
+  //         first_name: "Cameron",
+  //         last_name: "str",
+  //         clubs_house: "Almeida",
+  //         individual_role: "str",
+  //         projects: [
+  //           { project_id: "int", role: "int" },
+  //           { project_id: "int", role: "int" },
+  //         ],
+  //       },
+  //       {
+  //         id: 2,
+  //         first_name: "Cameron",
+  //         last_name: "str",
+  //         clubs_house: "Somerville",
+  //         individual_role: "str",
+  //         projects: [
+  //           { project_id: "int", role: "int" },
+  //           { project_id: "int", role: "int" },
+  //         ],
+  //       },
+  //   ];
 
-// mock user data
-const users = [
-    {
-      id: 1,
-      first_name: "Caelyb",
-      last_name: "str",
-      clubs_house: "Somerville",
-      individual_role: "str",
-      projects: [
-        { project_id: "int", role: "int" },
-        { project_id: "int", role: "int" },
-      ],
-    },
-    {
-      id: 2,
-      first_name: "Andrew",
-      last_name: "str",
-      clubs_house: "Almeida",
-      individual_role: "str",
-      projects: [
-        { project_id: "int", role: "int" },
-        { project_id: "int", role: "int" },
-      ],
-    },
-    {
-        id: 2,
-        first_name: "Cameron",
-        last_name: "str",
-        clubs_house: "Almeida",
-        individual_role: "str",
-        projects: [
-          { project_id: "int", role: "int" },
-          { project_id: "int", role: "int" },
-        ],
-      },
-      {
-        id: 2,
-        first_name: "Cameron",
-        last_name: "str",
-        clubs_house: "Somerville",
-        individual_role: "str",
-        projects: [
-          { project_id: "int", role: "int" },
-          { project_id: "int", role: "int" },
-        ],
-      },
-  ];
+  function userAdd(user) {
+    handleClick(user);
+    // this function would ideally take user id and export so use could be added to a project
+    return console.log(user.id);
+  }
 
-function userAdd(user) {
-handleClick(user)
-// this function would ideally take user id and export so use could be added to a project
-return console.log(user.id)
-}
-
-const handleInput = (e) => {
+  const handleInput = (e) => {
     e.preventDefault();
     setSearchInput(e.target.value);
   };
 
+  // could make this so table updates while user typing
+  const handleChange = () => {
+    if (searchInput.length > 0) {
+      // this filters based on search input and put filtered data into a list
 
-// could make this so table updates while user typing
-const handleChange = () => {
+      setUserToDisplay(
+        users.users.filter((user) => {
+          return user.name.toUpperCase().match(searchInput.toUpperCase());
+        })
+      );
+    }
+  };
 
-if (searchInput.length > 0) {
-// this filters based on search input and put filtered data into a list
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="Search here"
+        onChange={handleInput}
+        value={searchInput}
+      />
 
-        setUserToDisplay(
-            users.filter((user) => {
-    return (user.first_name.toUpperCase())
-    .match(searchInput.toUpperCase());
-})); 
-}}
+      <button type="submit" onClick={handleChange}>
+        Search
+      </button>
 
-
-return <div>
-    
-<input
-   type="search"
-   placeholder="Search here"
-   onChange={handleInput}
-   value={searchInput} />
-
-   <button type="submit" onClick={handleChange}> Search</button>
-
-<table>
-  <thead>
-    <tr>
-    <th> Name </th>
-    <th> Club </th>
-    </tr>
-  </thead>
-<tbody>
-
-
-  {usersToDisplay.map((user, index) => {
-    return (
-        <tr key = {index} >
-            <td > {user.first_name} </td>
-            <td > {user.clubs_house} </td>
-            <td> <button type = "submit" onClick={()=>userAdd(user)}>  Add User </button> </td>
-        </tr>
-)
-})}
-
-</tbody>
-</table>
-
-</div>
-
-
+      <table>
+        <thead>
+          <tr>
+            <th> Name </th>
+            <th> Club </th>
+          </tr>
+        </thead>
+        <tbody>
+          {usersToDisplay.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td> {user.name} </td>
+                <td> {user.clubs_house} </td>
+                <td>
+                  <button type="submit" onClick={() => userAdd(user)}>
+                    Add User
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default SearchBar;
