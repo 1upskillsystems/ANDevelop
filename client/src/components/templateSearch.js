51a4fff34627f5f3ad31680282febeb996415f89
caitlinrwAND <caitlin.whiteley@and.digital>
2022-10-28 10:01:54 +0100

    style the template search page

--- client/src/components/templateSearch.js
+++ client/src/components/templateSearch.js
@@ -6,8 +6,8 @@ import Button from "react-bootstrap/Button";
 import "./css/templateSearch.css";
 
 function TemplateSearch() {
-  const temp = useContext(TemplateContext);
-  let [templateObj, setTemplateObj] = useState([]);
+  const { templates } = useContext(TemplateContext);
+  let [templateObj, setTemplateObj] = useState();
 
   return (
     <PageContainer
@@ -16,51 +16,54 @@ function TemplateSearch() {
       buttonTo="/choose-template-route"
     >
       <section className="template-search">
-        <h2>Please select a template to start your project with</h2>
+        <h2 id="template-search-heading">
+          Please select a template to start your project with
+        </h2>
         <div className="template-search-viewport">
           <ul className="template-search-list">
-            {temp.templates.map((elem, i) => {
-              return (
-                <li
-                  className={`template-search-item ${
-                    templateObj.id === elem.id ? "selected" : ""
-                  }`}
-                  onClick={() => setTemplateObj((templateObj = elem))}
-                  key={i}
-                >
-                  <h4>{elem.template_name}</h4>
-                </li>
-              );
-            })}
+            {templates.map((elem, i) => (
+              <li
+                className={`template-search-item ${
+                  templateObj?.id === elem.id ? "selected" : ""
+                }`}
+                onClick={() => setTemplateObj((templateObj = elem))}
+                key={i}
+              >
+                <button>{elem.template_name}</button>
+              </li>
+            ))}
           </ul>
         </div>
-        <div className="template-search-details">
-          <div className={`wrapper${templateObj === 0 ? " hidden" : ""}`}>
-            <h2>{templateObj.template_name}</h2>
-            <p>{templateObj.desciption}</p>
-            <p>
-              <strong>Difficulty:</strong> {templateObj.difficulty_rating}
-            </p>
-            <hr />
-            <h2>Recommendations</h2>
-            <p>
-              <strong>Expected duration:</strong> {templateObj.time_esitmate}
-            </p>
-            <p>
-              <strong>Team size:</strong> {templateObj.team_estimate}
-            </p>
-          </div>
-        </div>
-        <footer>
-          <NavLink to={"/create-project"} state={{ templateInfo: templateObj }}>
-            <Button
-              variant="dark"
-              disabled={templateObj === 0 ? "disabled" : ""}
+        {!!templateObj && (
+          <div className="template-search-details">
+            <div className={`wrapper${templateObj === 0 ? " hidden" : ""}`}>
+              <h2>{templateObj.template_name}</h2>
+              <p>{templateObj.desciption}</p>
+              <p>
+                <strong>Difficulty:</strong> {templateObj.difficulty_rating}
+              </p>
+              <hr />
+              <h2>Recommendations</h2>
+              <p>
+                <strong>Expected duration:</strong> {Math.round(templateObj.time_esitmate / 86400)} days
+              </p>
+              <p>
+                <strong>Team size:</strong> {templateObj.team_estimate}
+              </p>
+            </div>
+            <NavLink
+              to={"/create-project"}
+              state={{ templateInfo: templateObj }}
             >
-              Assemble project
-            </Button>
-          </NavLink>
-        </footer>
+              <Button
+                variant="dark"
+                disabled={templateObj === 0 ? "disabled" : ""}
+              >
+                Assemble project
+              </Button>
+            </NavLink>
+          </div>
+        )}
       </section>
     </PageContainer>
   );
